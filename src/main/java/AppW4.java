39b085a06fa126556cd2bef9ca6b5bb848c7fcc5
alexrezv <ramspb@niuitmo.ru>
2018-07-02 11:41:17 +0300

    Week 4 initial commit

---
+++ src/main/java/AppW4.java
@@ -0,0 +1,11 @@
+public class AppW4 {
+    public static void main(String[] args) {
+        System.out.println(new AppW4().getGreeting());
+
+    }
+
+    String getGreeting() {
+        return "\n=== Course 5 Week 2 ===\n";
+    }
+}
+
